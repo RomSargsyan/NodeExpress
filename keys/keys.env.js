d00c990c9d35b0213c87280d7498f17835fd3395
RomSargsyan <romsargsyan20@gmail.com>
2020-08-21 21:21:22 +0400

    Fixes

--- keys/keys.env.js
+++ keys/keys.env.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MONGOGB_URI: 'mongodb+srv://RomSargsyan:HvcWwxiKztKjgEgL@cluster0-exdtt.mongodb.net/shop',
+    MONGOGB_URI: 'mongodb+srv://RomSargsyan:1234qwer@cluster0.ue71e.mongodb.net/RomSargsyan?retryWrites=true&w=majority',
     SESSION_SECRET: 'keyboard cat',
     API_KEY_SENDGRID: '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',
     BASE_URL: 'http://localhost:3000/'
